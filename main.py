e87b3229bc09325617cb438fc89929df7a2046f1
Kat <askfriends@live.com.pk>
2023-03-08 02:47:51 +0500

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import PyBypass as bypasser
 
 st.set_page_config(page_title="URL Bypasser", page_icon='🧊',
                    layout="centered", initial_sidebar_state="auto",    menu_items={
-                       'Get Help': 'https://telegram.me',
-                       'Report a bug': "Contact us askfriends1@protonmail.com",
+                       'Get Help': 'askfriends1@protonmail.com',
+                       'Report a bug': "https://telegram.me",
                        'About': "This is URL Bypasser website."
                    })
 
