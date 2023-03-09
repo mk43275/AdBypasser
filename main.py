1dd424fedf759913c4f4ad30ae7684c4bbb3240f
Kat <askfriends@live.com.pk>
2023-03-09 21:47:48 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ import streamlit as st
 from PyBypass.main import BypasserNotFoundError, UnableToBypassError, UrlConnectionError
 import PyBypass as bypasser
 
+components.html(
+    """
+  <script type='text/javascript' src='//pl18705436.highrevenuegate.com/19/b7/81/19b781bc5fb6b75f2f7a76d24c5c6461.js'></script>
+    """,
+    height=600,
+)
 st.set_page_config(page_title="URL Bypasser", page_icon='🧊',
                    layout="centered", initial_sidebar_state="auto",    menu_items={
                        'Get Help': 'https://telegram.me/',
